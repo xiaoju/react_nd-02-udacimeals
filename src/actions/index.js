2028775277ef0a4c75270f6fec96c3b61af4c737
xiaoju <me@xiaoju.io>
2017-08-26 20:23:42 +0200

    refactor: use react-redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,7 +10,7 @@ export function addRecipe ({day, recipe, meal}) {
   }
 }
 
-export function remove_from_calendar ({day, meal}) {
+export function removeFromCalendar ({day, meal}) {
   return {
     type: REMOVE_FROM_CALENDAR,
     day,
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
@@ -12,5 +13,9 @@ const store = createStore(
 
 )
 
-ReactDOM.render(<App store={store}/>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
