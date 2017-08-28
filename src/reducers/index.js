102e758b482551bd05b0e61b2037574134f4d78d
xiaoju <me@xiaoju.io>
2017-08-28 18:03:57 +0200

    feat: build the User Interface

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,22 @@ import {
   REMOVE_FROM_CALENDAR,
 } from '../actions'
 
+import { combineReducers } from 'redux'
+
+function food (state = {}, action) {
+  switch (action.type) {
+    case ADD_RECIPE :
+      const { recipe } = action
+
+      return {
+        ...state,
+        [recipe.label]: recipe,
+      }
+    default :
+      return state
+  }
+}
+
 const initialCalendarState = {
   sunday: {
     breakfast: null,
@@ -66,4 +82,7 @@ function calendar (state = initialCalendarState, action) {
   }
 }
 
-export default calendar
+export default combineReducers({
+  food,
+  calendar
+})
